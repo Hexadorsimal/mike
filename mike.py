a34e1c74c59fc55fa4c87278e2021a86693b06f5
John Whipple <john@whipple.org>
2017-06-20 09:08:57 -0400

    initial commit

---
+++ mike.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+from tempfile import TemporaryDirectory
+from flask import Flask, render_template, send_file
+from flask_bootstrap import Bootstrap
+from submit_form import SubmitForm
+
+
+app = Flask(__name__)
+Bootstrap(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = SubmitForm()
+    if form.validate_on_submit():
+        with TemporaryDirectory() as tempdir:
+            output_filename = run_script(tempdir)
+            return send_file(output_filename, as_attachment=True)
+
+    return render_template("index.html", form=form)
+
+
+def run_script(dirname):
+    output = subprocess.check_output(["echo", dirname])
+
+    filename = os.path.join(dirname, "echo.txt")
+    with open(filename, "wb+") as f:
+        f.write(output)
+
+    return filename
+
+
+if __name__ == '__main__':
+    app.run()
